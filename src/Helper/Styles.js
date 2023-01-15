b154cc14c58e8efe01367e993e8350116ee80e48
Wev-Dev-Bibhu <iambibhutidas@gmail.com>
2023-01-16 01:33:54 +0530

    update v1

--- src/Helper/Styles.js
+++ src/Helper/Styles.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://image-blog.onrender.com"
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = "https://image-blog.onrender.com"
+// export const BASE_URL = "http://localhost:5000"
 
 export const formStyle = {
     background: '#112132',
