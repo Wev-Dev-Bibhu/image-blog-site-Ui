cd479b32825006c10089f2a6bcfe4a3b77fbf3cd
Wev-Dev-Bibhu <iambibhutidas@gmail.com>
2023-02-01 02:16:03 +0530

    NA

--- src/Helper/Common.js
+++ src/Helper/Common.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://image-blog.onrender.com"
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = "https://image-blog.onrender.com"
+// export const BASE_URL = "http://localhost:5000"
 export const handleDescription = (description) => {
     if (description) {
         if (description.length > 42) {
